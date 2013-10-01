8422037f020ccd1255ebf1c4b9d0e3016c27d8aa
Randall Smith <rsmith0x0@gmail.com>
2013-10-01 14:22:44 -0500

    image stuff working now

--- tests/image_storage_test.cpp
+++ tests/image_storage_test.cpp
@@ -42,22 +42,47 @@ TEST(ImageStorageTest, ImageWriteTest) {
     }
     bool successful_write = storage.WriteImage("../assets/colors.jpg", image,
             status);
-    std::cout << "STATUS=" << status << std::endl;
     EXPECT_TRUE(successful_write);
 }
 TEST(ImageStorageTest, ImageReadWrite) {
     image_utils::ImageStorage& storage =
             image_utils::ImageStorage::GetInstance();
-    image_utils::Image image;
+    image_utils::Image image_src;
+
     std::string src_path = "../assets/checkerboard.jpg";
     std::string dest_path = "../assets/checkerboard_copy.jpg";
     std::string status = "";
-    bool success = storage.ReadImage(src_path, image, status);
+
+    // load the checkerboard
+    bool success = storage.ReadImage(src_path, image_src, status);
     EXPECT_TRUE(success);
     EXPECT_EQ("OK", status);
-    bool successful_write = storage.WriteImage(dest_path, image, status);
+
+    // copy it to another image
+    bool successful_write = storage.WriteImage(dest_path, image_src, status);
     EXPECT_EQ("OK", status);
     EXPECT_TRUE(successful_write);
+
+    // read result back in
+    image_utils::Image image_dest;
+    success = storage.ReadImage(dest_path, image_dest, status);
+    EXPECT_EQ("OK", status);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(image_dest.width(), image_src.width());
+    EXPECT_EQ(image_dest.height(), image_src.height());
+    float sum_error = 0;
+    float error = 0;
+    float size = static_cast<float>(image_dest.width() * image_dest.height());
+    float thresh = size * 0.005;
+    for (uint32_t i = 0; i < image_src.height(); ++i) {
+        for (uint32_t j = 0; j < image_src.width(); ++j) {
+            glm::vec3 pixel_src = static_cast<glm::vec3>(image_src(i, j));
+            glm::vec3 pixel_dest = static_cast<glm::vec3>(image_dest(i, j));
+            error = glm::distance(pixel_src, pixel_dest);
+            sum_error += error;
+        }
+    }
+    EXPECT_LT(error, thresh);
 }
 }
 
