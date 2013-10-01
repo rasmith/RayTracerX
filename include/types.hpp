6695288d0dcd54b2e9ee5f3d8cf30611d370c70a
Randall Smith <rsmith0x0@gmail.com>
2013-10-01 14:25:13 -0500

    image deps

---
+++ include/types.hpp
@@ -0,0 +1,19 @@
+/*
+ * types.hpp
+ *
+ *  Created on: Sep 30, 2013
+ *      Author: agrippa
+ */
+
+#ifndef TYPES_HPP_
+#define TYPES_HPP_
+
+#define GLM_FORCE_PURE
+#define GLM_FORCE_CXX98
+#include <glm/glm.hpp>
+
+namespace ray {
+typedef glm::detail::tvec3<unsigned char> ucvec3;
+}
+
+#endif /* TYPES_HPP_ */
