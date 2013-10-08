5a973b07db2cec8e335981013362484d05258f29
Randall Smith <rsmith0x0@gmail.com>
2013-10-08 14:08:29 -0500

    camera changes

--- include/camera.hpp
+++ include/camera.hpp
@@ -9,16 +9,51 @@
 #define CAMERA_HPP_
 #include "types.hpp"
 namespace ray {
-struct Camera {
-    float aspect;
-    float near;
-    float far;
-    float fov;
-    std::string name;
-    glm::vec3 at;
-    glm::vec3 position;
-    glm::vec3 up;
-    glm::mat4x4 transform;
+class Camera {
+public:
+    Camera();
+    Camera(const Camera& cam);
+    Camera(float near, float far, float aspect, float fov,
+            const glm::vec3& at, const glm::vec3& position,
+            const glm::vec3& up);
+    glm::vec4 Project(const glm::vec3& point);
+    glm::vec4 Unproject(const glm::vec3& point);
+
+    // getters/setters
+    float aspect() const;
+    void set_aspect(float aspect);
+    const glm::vec3& at() const;
+    void set_at(const glm::vec3& at);
+    float far() const;
+    void set_far(float far);
+    float fov() const;
+    void set_fov(float fov);
+    const std::string& name() const;
+    void set_name(const std::string& name);
+    float near() const;
+    void set_near(float near);
+    const glm::vec3& position() const;
+    void set_position(const glm::vec3& position);
+    const glm::mat4x4& transform() const;
+    void set_transform(const glm::mat4x4& transform);
+    const glm::vec3& up() const;
+    void set_up(const glm::vec3& up);
+    const glm::mat4x4& project() const;
+    const glm::mat4x4& unproject() const;
+    const glm::mat4x4& view() const;
+private:
+    float near_;
+    float far_;
+    float aspect_;
+    float fov_;
+    std::string name_;
+    glm::vec3 at_;
+    glm::vec3 position_;
+    glm::vec3 up_;
+    glm::mat4x4 transform_;
+    glm::mat4x4 view_;
+    glm::mat4x4 project_;
+    glm::mat4x4 unproject_;
 };
 } // namespace ray
 #endif /* CAMERA_HPP_ */
--- src/camera.cpp
+++ src/camera.cpp
@@ -4,9 +4,117 @@
  *  Created on: Oct 3, 2013
  *      Author: agrippa
  */
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include "camera.hpp"
 namespace ray {
+Camera::Camera() : near_(0.0f), far_(0.0f), aspect_(0.0f), fov_(0.0f),
+        at_(0.0f, 0.0f, 0.0f), position_(0.0f, 0.0f, 0.0f),
+        up_(0.0f, 0.0f, 0.0f) {
+}
+
+Camera::Camera(const Camera& cam)  : near_(cam.near_), far_(cam.far_),
+        aspect_(cam.aspect_), fov_(cam.fov_), at_(cam.at_),
+        position_(cam.position_), up_(cam.up_) {
+    project_ = glm::perspective(fov_, aspect_, near_, far_);
+    unproject_  = glm::inverse(project_);
+}
+
+Camera::Camera(float near, float far, float aspect, float fov,
+           const glm::vec3& at, const glm::vec3& position,
+           const glm::vec3& up) : near_(near), far_(far), aspect_(aspect),
+                    fov_(fov), at_(at), position_(position), up_(up) {
+}
+
+float Camera::aspect() const {
+    return aspect_;
+}
+
+void Camera::set_aspect(float aspect) {
+    aspect_ = aspect;
+}
+
+const glm::vec3& Camera::at() const {
+    return at_;
+}
+
+void Camera::set_at(const glm::vec3& at) {
+    at_ = at;
+}
+
+float Camera::far() const {
+    return far_;
+}
+
+void Camera::set_far(float far) {
+    far_ = far;
+}
+
+float Camera::fov() const {
+    return fov_;
+}
+
+void Camera::set_fov(float fov) {
+    fov_ = fov;
+}
+
+const std::string& Camera::name() const {
+    return name_;
+}
+
+void Camera::set_name(const std::string& name) {
+    name_ = name;
+}
+
+float Camera::near() const {
+    return near_;
+}
+
+void Camera::set_near(float near) {
+    near_ = near;
+}
+
+const glm::vec3& Camera::position() const {
+    return position_;
+}
+
+void Camera::set_position(const glm::vec3& position) {
+    position_ = position;
+}
+
+const glm::mat4x4& Camera::transform() const {
+    return transform_;
+}
+
+void Camera::set_transform(const glm::mat4x4& transform) {
+    transform_ = transform;
+}
+
+const glm::vec3& Camera::up() const {
+    return up_;
+}
+
+glm::vec4 Camera::Project(const glm::vec3& point) {
+}
+
+glm::vec4 Camera::Unproject(const glm::vec3& point) {
+}
+
+void Camera::set_up(const glm::vec3& up) {
+    up_ = up;
+}
+
+const glm::mat4x4& Camera::project() const {
+    return project_;
+}
+
+const glm::mat4x4& Camera::unproject() const {
+    return unproject_;
+}
+
+const glm::mat4x4& Camera::view() const {
+    return view_;
+}
+
 } // namespace ray
-
-
 
