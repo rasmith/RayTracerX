17ab3d7defd9e91b917edc9c20c8e0dd746c97e5
Randall Smith <rsmith.0x0@gmail.com>
2013-10-23 16:23:06 -0500

    scene utils

---
+++ include/scene_utils.hpp
@@ -0,0 +1,22 @@
+/*
+ * scene_utils.hpp
+ *
+ *  Created on: Oct 22, 2013
+ *      Author: agrippa
+ */
+
+#ifndef SCENE_UTILS_HPP_
+#define SCENE_UTILS_HPP_
+#include "scene.hpp"
+#include "types.hpp"
+namespace ray {
+class SceneLoader {
+public:
+    static SceneLoader& GetInstance();
+    ~SceneLoader();
+    bool LoadScene(const std::string& file_name, const Scene& image,
+                std::string& status);
+
+};
+}
+#endif /* SCENE_UTILS_HPP_ */
