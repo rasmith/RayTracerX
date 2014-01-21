6f7dc7210097bf364fc9352a8a1171fa9e8d1297
Randall Smith <rsmith.0x0@gmail.com>
2014-01-21 10:57:29 -0600

    octree stuff

--- include/octree.hpp
+++ include/octree.hpp
@@ -8,18 +8,33 @@
 #ifndef OCTREE_HPP_
 #define OCTREE_HPP_
 #include <algorithm>
+#include <cstring>
 #include <list>
 #include <vector>
 #include "shape.hpp"
 namespace ray {
-template<class SceneObject>
-class OctTree: public Shape {
+class OctTreeBase : public Shape {
+public:
+	static const int kMaxDepth;
 private:
+	OctTreeBase();
+};
+template<class SceneObject>
+class OctTree: public OctTreeBase {
+private:
+	// OctIndex -
+	// 31      : leaf or internal node
+	// 30 - 27 : 4 bits, number of children or scene objects, 16 max.
+	// 26 - 0  : 27 bits, index into corresponding array
+	//		   : (node, leaf, index). Approx. 134M possible indices.
+	typedef unsigned int OctIndex;
+	typedef unsigned int ObjectIndex;
 	typedef std::vector<SceneObject*> ObjectVector;
 	typedef std::list<SceneObject*> ObjectList;
 	struct OctNode {
-		BoundingBox bounds;
-		OctNode children[8];
+		OctIndex index;
+		OctNode() : index(0) {
+		}
 		bool Intersect(const Ray& ray, Isect& isect) const {
 			float t_near = -std::numeric_limits<float>::max();
 			float t_far = std::numeric_limits<float>::max();
@@ -29,9 +44,8 @@ private:
 		}
 	};
 	struct OctLeaf {
-		ObjectList* objects;
-		bool Intersect(const Ray& ray, Isect& isect) const {
-			const ObjectList& objs = *objects;
+		ObjectIndex index;
+		bool Intersect(const ObjectList* objs, const Ray& ray, Isect& isect) const {
 			bool hit = false;
 			Isect current, best;
 			best.t_hit = std::numeric_limits<float>::max();
@@ -50,15 +64,34 @@ private:
 	};
 public:
 	virtual ~OctTree();
-	virtual bool Intersect(const Ray& ray, Isect& isect) const = 0;
+	virtual bool Intersect(const Ray& ray, Isect& isect) const {
+		return Traverse(root_index_, ray, isect, 0);
+	}
 	virtual BoundingBox GetBounds() = 0;
 	virtual void Print(std::ostream& out) const;
 	void Build(const ObjectVector& objects) {
 
 	}
 private:
+	std::vector<OctNode> nodes_;
+	std::vector<OctIndex> indices_;
+	std::vector<OctLeaf> leaves_;
+	ObjectVector scene_objects_;
+	const OctNode& GetNodeConstRef(const OctIndex& index) const {
+		return root_index_;
+	}
+	OctIndex root_index_;
+	BoundingBox root_bounds_;
+	bool Traverse(const OctIndex& node_index, const Ray& ray, Isect& isect,
+			int depth) const {
+		const OctNode& node = GetNodeConstRef(node_index);
+		if (!node.Intersect(ray, isect))
+			return false;
+		return true;
+	}
 	void BuildRecursive(ObjectVector& objects);
 };
+
 }
 
 #endif /* OCTREE_HPP_ */
