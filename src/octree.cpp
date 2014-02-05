7664b51bd0896110f49c0648482e9434f3ffdd2f
Randall Smith <rsmith0x0@gmail.com>
2014-02-05 14:41:26 -0600

    testing octree

---
+++ src/octree.cpp
@@ -0,0 +1,11 @@
+/*
+ * octree.cpp
+ *
+ *  Created on: Feb 5, 2014
+ *      Author: agrippa
+ */
+
+namespace ray {
+const int OctTreeBase::kMaxDepth = 20;
+const int OctTreeBase::kMaxLeafSize = 16;
+}
---
+++ tests/octree_test.cpp
@@ -0,0 +1,23 @@
+/*
+ * raytracer_test.cpp
+ *
+ *  Created on: Nov 27, 2013
+ *      Author: agrippa
+ */
+
+#include <climits>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "octree.hpp"
+namespace ray {
+TEST(OctreeTest, NodeEncodeTest) {
+
+}
+} // namespace ray
+
