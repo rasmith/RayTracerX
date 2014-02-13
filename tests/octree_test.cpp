ccd0ffba69499927badd6e736083f87bd6e9c362
Randall Smith <rsmith.0x0@gmail.com>
2014-02-13 11:36:27 -0600

    testing for regular octree

--- tests/octree_test.cpp
+++ tests/octree_test.cpp
@@ -47,5 +47,55 @@ TEST(OctreeTest, NodeEncodeTest) {
     EXPECT_EQ(internal_node.size(), test_node.size());
   }
 }
+
+TEST(RayTracerTest, BunnyMeshTest) {
+  SceneLoader& loader = SceneLoader::GetInstance();
+  std::string path = "../assets/bunny.obj";
+  std::string status = "";
+  Scene scene;
+  std::cout << "loading" << std::endl;
+  bool success = loader.LoadScene(path, scene, status);
+  EXPECT_TRUE(success);
+  EXPECT_EQ("OK", status);
+  int image_width = 512;
+  int image_height = 512;
+  glm::vec3 eye_pos = glm::vec3(0.0f, 0.1f, 0.5f);
+  glm::vec3 at_pos = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 up_dir = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::mat4x4 look_at = LookAt(eye_pos, at_pos, up_dir);
+  Camera camera(image_width, image_height, Orthographic(0.0f, 1.0f), look_at);
+
+  Light point_light;
+  glm::vec3 point_light_color = glm::vec3(1.0f, 0.3f, 0.3f);
+  point_light.ka = point_light_color;
+  point_light.kd = point_light_color;
+  point_light.ks = point_light_color;
+  point_light.ray = Ray(glm::vec3(0.0, 1.0, 2.0f), glm::vec3(0.0f));
+  point_light.type = Light::kPoint;
+  point_light.attenuation_coefficients = glm::vec3(0.25f, 0.003372407f,
+      0.000045492f);
+
+  Light directional_light;
+  glm::vec3 directional_light_color = glm::vec3(0.2f, 0.2f, 0.2f);
+  directional_light.ka = directional_light_color;
+  directional_light.kd = directional_light_color;
+  directional_light.ks = directional_light_color;
+  directional_light.ray = Ray(glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+  directional_light.type = Light::kDirectional;
+
+  scene.AddLight(point_light);
+  scene.AddLight(directional_light);
+
+  Image image;
+  image.resize(image_width, image_height);
+  RayTracer ray_tracer(&scene, &camera);
+
+  ray_tracer.Render(image);
+  ImageStorage& storage = ImageStorage::GetInstance();
+  success = storage.WriteImage("bunny_octree.jpg", image, status);
+  EXPECT_TRUE(success);
+  EXPECT_EQ("OK", status);
+}
+}
 } // namespace ray
 
