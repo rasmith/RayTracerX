4c5b77ce9ced7508c30b8a381bb7759f096d45ce
Randall Smith <rsmith.0x0@gmail.com>
2014-04-23 11:58:38 -0500

    kd node encoding

---
+++ include/kdnode64.hpp
@@ -0,0 +1,84 @@
+/*
+ * kdnode64.hpp
+ *
+ *  Created on: Apr 22, 2014
+ *      Author: randallsmith
+ */
+
+#ifndef KDNODE64_HPP_
+#define KDNODE64_HPP_
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <ostream>
+#include "types.hpp"
+namespace ray {
+class KdNode64 {
+public:
+  enum NodeType {
+    kSplitX = 0, kSplitY, kSplitZ, kLeaf
+  };
+  KdNode64();
+  KdNode64(const KdNode64& node);
+  KdNode64(const KdNode64::NodeType& node_type, uint32_t node_size,
+      uint32_t node_offset, float split_value);
+  bool IsLeaf() const;
+  bool IsInternal() const;
+  NodeType type() const;
+  void set_type(NodeType type);
+  uint32_t index() const;
+  void set_index(uint32_t index);
+  uint32_t size() const;
+  void set_size(uint32_t size);
+  uint32_t offset() const;
+  void set_offset(uint32_t offset);
+  float split_value() const;
+  void set_split_value(float value);
+  bool operator==(const KdNode64& node) const;
+  KdNode64& operator=(const KdNode64& node);
+private:
+  NodeType type_;
+  uint32_t index_;
+  uint32_t size_;
+  uint32_t offset_;
+  float split_value_;
+};
+
+std::ostream& operator<<(std::ostream& out, const KdNode64& node);
+
+// data[0]: top bit is type
+//      : next two bits indicate split dim
+//      : bottom four bits unused
+// data[1..4]: offset into corresponding array, 32 bits, max size 4,294,967,296
+// data[5..7]: node size, 24 bits, max size 16,777,216, empty nodes not stored
+struct EncodedKdNode64 {
+  u_char data[8];
+  EncodedKdNode64();
+  EncodedKdNode64(const EncodedKdNode64& node);
+  EncodedKdNode64& operator=(const EncodedKdNode64& node);
+  KdNode64::NodeType GetType() const;
+  uint32_t GetIndex() const;
+  uint32_t GetSize() const;
+  uint32_t GetOffset() const;
+  float GetSplitValue() const;
+  void SetType(KdNode64::NodeType type);
+  void SetIndex(uint32_t index);
+  void SetSize(uint32_t size);
+  void SetOffset(uint32_t offset);
+  void SetSplitValue(float value);
+};
+
+std::ostream& operator<<(std::ostream& out, const EncodedKdNode64& scene);
+
+class KdNodeFactory64 {
+public:
+  static KdNodeFactory64& GetInstance();
+  KdNode64 CreateOctNode(const EncodedKdNode64& encoded) const;
+  KdNode64 CreateLeaf(uint32_t octant) const;
+  KdNode64 CreateInternal(uint32_t octant) const;
+  EncodedKdNode64 CreateEncodedNode(const KdNode64& node) const;
+private:
+  KdNodeFactory64();
+};
+} // namespace ray
+#endif /* KDNODE64_HPP_ */
---
+++ src/kdnode64.cpp
@@ -0,0 +1,204 @@
+/*
+ * kdnode64.cpp
+ *
+ *  Created on: Apr 23, 2014
+ *      Author: randallsmith
+ */
+#include <stdint.h>
+#include <iostream>
+#include "kdnode64.hpp"
+namespace ray {
+EncodedKdNode64::EncodedKdNode64() {
+  for (int i = 0; i < 8; ++i)
+    data[i] = 0;
+}
+
+EncodedKdNode64::EncodedKdNode64(const EncodedKdNode64& node) {
+  for (int i = 0; i < 8; ++i)
+    data[i] = node.data[i];
+}
+
+EncodedKdNode64& EncodedKdNode64::operator =(const EncodedKdNode64& node) {
+  if (this == &node)
+    return *this;
+  for (int i = 0; i < 8; ++i)
+    data[i] = node.data[i];
+  return *this;
+}
+
+KdNode64::NodeType EncodedKdNode64::GetType() const {
+  return static_cast<KdNode64::NodeType>((data[0] & 0xC0) >> 6);
+}
+
+uint32_t EncodedKdNode64::GetOffset() const {
+  uint32_t offset = 0x0;
+  uint32_t byte = 0x0;
+  for (uint32_t i = 1; i < 4; ++i) {
+    byte = static_cast<uint32_t>(data[i]);
+    offset = offset | (byte << ((i - 1) * 8));
+  }
+  return offset;
+}
+
+uint32_t EncodedKdNode64::GetSize() const {
+  uint32_t size = 0x0;
+  return size;
+}
+
+uint32_t EncodedKdNode64::GetIndex() const {
+  return (data[0] & 0x20)
+}
+
+float EncodedKdNode64::GetSplitValue() const {
+  float value;
+  float* src = reinterpret_cast<float *>(&data[4]);
+  float* dest = &value;
+  *dest = *src;
+  return value;
+}
+
+void EncodedKdNode64::SetType(KdNode64::NodeType type) {
+  u_char mask = static_cast<u_char>(type);
+  mask = (mask << 7) & 0x80;
+  data[0] = (0x7F & data[0]) | mask;
+}
+
+void EncodedKdNode64::SetIndex(uint32_t index) {
+}
+
+void EncodedKdNode64::SetSize(uint32_t size) {
+}
+
+void EncodedKdNode64::SetOffset(uint32_t offset) {
+  uint32_t byte = 0x0;
+  uint32_t shift = 0;
+  for (uint32_t i = 1; i < 4; ++i) {
+    shift = (i - 1) * 8;
+    byte = (offset >> shift) & 0x000000FF;
+    data[i] = static_cast<u_char>(byte);
+  }
+}
+
+void EncodedKdNode64::SetSplitValue(float value) {
+}
+
+std::ostream& operator<<(std::ostream& out, const EncodedKdNode64& node) {
+  for (uint32_t i = 0; i < 8; ++i) {
+    for (uint32_t j = 0; j < 8; ++j)
+      out << ((0x1 << (7 - j)) & node.data[i] ? '1' : '0');
+    out << ' ';
+  }
+  return out;
+}
+
+KdNode64::KdNode64() {
+}
+
+KdNode64::KdNode64(const KdNode64& node) {
+}
+
+KdNode64::KdNode64(const KdNode64::NodeType& node_type, uint32_t node_size,
+    uint32_t node_offset, float split_value) {
+}
+
+uint32_t KdNode64::index() const {
+  return index_;
+}
+
+void KdNode64::set_index(uint32_t index) {
+  index_ = index;
+}
+
+float KdNode64::split_value() const {
+  return split_value_;
+}
+
+void KdNode64::set_split_value(float value) {
+  split_value_ = value;
+}
+
+bool KdNode64::operator ==(const KdNode64& node) const {
+}
+
+KdNode64&KdNode64::operator=(const KdNode64&node) {
+  if (this == &node)
+    return *this;
+  type_ = node.type_;
+  size_ = node.size_;
+  offset_ = node.offset_;
+  return *this;
+}
+
+KdNodeFactory64::KdNodeFactory64() {
+}
+
+KdNodeFactory64 & KdNodeFactory64::GetInstance() {
+  static KdNodeFactory64 instance;
+  static bool is_initialized = false;
+  if (!is_initialized) {
+    instance = KdNodeFactory64();
+  }
+  return instance;
+}
+
+KdNode64 KdNodeFactory64::CreateOctNode(const EncodedKdNode64& encoded) const {
+  KdNode64 node;
+  node.set_size(encoded.GetSize());
+  node.set_type(encoded.GetType());
+  node.set_offset(encoded.GetOffset());
+  return node;
+}
+
+KdNode64 KdNodeFactory64::CreateLeaf(uint32_t octant) const {
+  return KdNode64(KdNode64::kLeaf, octant, 0, 0);
+}
+
+KdNode64 KdNodeFactory64::CreateInternal(uint32_t index) const {
+  return KdNode64(KdNode64::kSplitX, index, 0, 0);
+}
+
+EncodedKdNode64 KdNodeFactory64::CreateEncodedNode(const KdNode64& node) const {
+  EncodedKdNode64 encoded;
+  encoded.SetType(node.type());
+  encoded.SetOffset(node.offset());
+  encoded.SetSize(node.size());
+  return encoded;
+}
+
+uint32_t KdNode64::offset() const {
+  return offset_;
+}
+
+void KdNode64::set_offset(uint32_t offset) {
+  offset_ = offset;
+}
+
+uint32_t KdNode64::size() const {
+  return size_;
+}
+
+void KdNode64::set_size(uint32_t size) {
+  size_ = size;
+}
+
+KdNode64::NodeType KdNode64::type() const {
+  return type_;
+}
+
+void KdNode64::set_type(NodeType type) {
+  type_ = type;
+}
+
+bool KdNode64::IsLeaf() const {
+  return type_ == kLeaf;
+}
+
+bool KdNode64::IsInternal() const {
+  return type_ != kLeaf;
+}
+
+std::ostream& operator<<(std::ostream& out, const KdNode64&node) {
+  return out;
+}
+} // namespace ray
+
